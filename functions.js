0ddbb2ff8e7aee4d4cd9b55582b8e0a56e7e6619
St0neC0ldCrazy <128265203+St0neC0ldCrazy@users.noreply.github.com>
2023-10-17 20:31:47 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -4,9 +4,9 @@ function botaoHamburguer() {
     const botaoMenuImagem = document.getElementById('botao-menu-imagem');
 
     toggleButton.classList.toggle('collapsed');
-
     menuTopo.classList.toggle('active');
 
     const isActive = menuTopo.classList.contains('active');
-    botaoMenuImagem.src = isActive ? '../TPC_13/images/desktop/menu-burger/fechar.svg' : '../TPC_13/images/desktop/menu-burger/menu.svg';
+    
+    botaoMenuImagem.src = isActive ? 'images/desktop/menu-burger/fechar.svg' : 'images/desktop/menu-burger/menu.svg';
 }
